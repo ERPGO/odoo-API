e95be8b4ef0d784e66b5bc7f8693c26a56a8fa5e
nurlanf <nurlan.farajov@gmail.com>
2019-02-17 12:57:29 +0100

    add first api

---
+++ create_validate_invoice/create_invoice.py
@@ -0,0 +1,39 @@
+from variables import customer, product_name, product_price, invoice_line_description, currency_id
+
+url = "https://erpgo-gez.odoo.com"
+db = "erpgo-gez-prod-285440"
+username = 'admin'
+password = 'freebsd'
+
+import xmlrpc.client
+
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+uid = common.authenticate(db, username, password, {})
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url), allow_none=True)
+
+
+customer_search = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['name', '=', customer]]])
+
+if not customer_search:
+    customer_id = models.execute_kw(db, uid, password, 'res.partner', 'create', [{'name': customer}])
+else:
+    customer_id = customer_search[0]['id']
+
+product = models.execute_kw(db, uid, password, 'product.template', 'search_read', [[['name', '=', product_name]]])
+product_ids = []
+product_ids.append((0, 0,
+                    {'product_id': product[0]['id'], 'name': invoice_line_description, 'price_unit': product_price,
+                     'account_id': product[0]['property_account_income_id'][0]}))
+invoice = models.execute_kw(db, uid, password, 'account.invoice', 'create',
+                            [{'partner_id': customer_id, 'invoice_line_ids': product_ids, 'currency_id': currency_id}])
+
+
+def validate_invoice( invoice ):
+    models.execute_kw(db, uid, password, 'account.invoice', 'action_invoice_open', [invoice])
+
+
+try:
+    validate_invoice(invoice)
+except:
+    pass
+
---
+++ create_validate_invoice/variables.py
@@ -0,0 +1,5 @@
+customer = "Kerim Kerimov"
+product_name = "Flight"
+product_price = "3200"
+invoice_line_description = "54321 GYD to Krakow"
+currency_id = 2
