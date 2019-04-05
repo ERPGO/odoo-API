9512bd3a7efa47cb94849404fac97600bafb553e
jumshudsultan <cumshud@gmail.com>
2019-04-05 20:32:14 +0400

    Add analytic account

--- create_validate_invoice/create_invoice.py
+++ create_validate_invoice/create_invoice.py
@@ -1,7 +1,7 @@
-from variables import customer, product_name, product_price, invoice_line_description, currency_id
+from variables import customer, product_name, product_price, invoice_line_description, currency_id, RESERVATION, GROUP
 
-url = "https://erpgo-gez.odoo.com"
-db = "erpgo-gez-prod-285440"
+url = "https://erpgo-gez-prod-345771.dev.odoo.com"
+db = "erpgo-gez-prod-345771"
 username = 'admin'
 password = 'freebsd'
 
@@ -20,17 +20,25 @@ else:
     customer_id = customer_search[0]['id']
 
 product = models.execute_kw(db, uid, password, 'product.template', 'search_read', [[['name', '=', product_name]]])
+
 product_ids = []
+group = models.execute_kw(db, uid, password, 'account.analytic.group', 'search_read', [[['name', '=', GROUP]]])
+analytic_search = models.execute_kw(db, uid, password, 'account.analytic.account', 'search_read', [[['name', '=', RESERVATION]]])
+if not analytic_search:
+    analytic_id = models.execute_kw(db, uid, password, 'account.analytic.account', 'create',
+                                    [{'name': RESERVATION, 'partner_id': customer_id, 'group_id': group[0]['id']}])
+else:
+    analytic_id = analytic_search[0]['id']
+
 product_ids.append((0, 0,
-                    {'product_id': product[0]['id'], 'name': invoice_line_description, 'price_unit': product_price,
+                    {'product_id': product[0]['id'], 'account_analytic_id': analytic_id, 'name': invoice_line_description, 'price_unit': product_price,
                      'account_id': product[0]['property_account_income_id'][0]}))
+
 invoice = models.execute_kw(db, uid, password, 'account.invoice', 'create',
                             [{'partner_id': customer_id, 'invoice_line_ids': product_ids, 'currency_id': currency_id}])
 
-
 def validate_invoice( invoice ):
     models.execute_kw(db, uid, password, 'account.invoice', 'action_invoice_open', [invoice])
-
 
 try:
     validate_invoice(invoice)
--- create_validate_invoice/variables.py
+++ create_validate_invoice/variables.py
@@ -1,5 +1,8 @@
 customer = "Kerim Kerimov"
-product_name = "Flight"
+product_name = "ticket"
 product_price = "3200"
 invoice_line_description = "54321 GYD to Krakow"
 currency_id = 2
+RESERVATION = 'RES12345'
+RESERVATION_TYPE = 'ticket'
+GROUP = 'website'
